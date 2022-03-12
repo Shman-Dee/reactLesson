5d89f44f6e99fb3f4a740bdbd1bd58399bc66ddb
Andrew McInally <realfunnyestate@yahoo.com>
2022-03-12 11:33:20 -0700

    activity4

---
+++ andyActivities/App.js
@@ -0,0 +1,22 @@
+import HelloWorld from "./components/HelloWorld";
+
+// export const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>hello world</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+export const App = () => {
+  return (
+    <div>
+      <HelloWorld />
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
---
+++ andyActivities/components/HelloWorld.js
@@ -0,0 +1,18 @@
+//all functional compnents needs to return html
+
+export const HelloWorld = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>my name is andy</p>
+      <p>these are my favorite cryptos</p>
+      <ul>
+        <li>btc</li>
+        <li>eth</li>
+        <li>mana</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HelloWorld;
